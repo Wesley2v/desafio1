9fa96e4f40c7b29fed2d2fd9210897c69b1a5829
Wesley Sousa <wesleyltsousa@gmail.com>
2025-08-28 11:01:14 -0300

    Project created

---
+++ src/test/java/Wesley2v/com/github/desafio1/Desafio1ApplicationTests.java
@@ -0,0 +1,13 @@
+package Wesley2v.com.github.desafio1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
