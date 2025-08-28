e04a313530c2c609008d7a7cb9d8b265328aee10
Wesley Sousa <wesleyltsousa@gmail.com>
2025-08-28 11:08:24 -0300

    Services order and shipping

---
+++ src/main/java/Wesley2v/com/github/desafio1/services/OrderService.java
@@ -0,0 +1,16 @@
+package Wesley2v.com.github.desafio1.services;
+
+import Wesley2v.com.github.desafio1.entities.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private ShippingService shippingService;
+
+    public Double total(Order order){
+        return order.getBasic() - (order.getBasic()) * order.getDiscount() / 100 + shippingService.shipment(order);
+    }
+}
